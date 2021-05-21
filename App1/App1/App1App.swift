a273202fc395fee04984cdbeafddc5522b4b4b3c
Adil Hussain <adilson05uk@gmail.com>
2021-05-21 18:19:55 +0100

    (App1) Created a simple SwiftUI application by means of the Xcode "Create project" template.

---
+++ App1/App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Adil Hussain on 21/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App1/App1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App1
+//
+//  Created by Adil Hussain on 21/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
