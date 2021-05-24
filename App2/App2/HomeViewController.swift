fd4db8dbe7c01fd2988b1b0fa224abc6a6eb16ce
Adil Hussain <adilson05uk@gmail.com>
2021-05-24 10:47:35 +0100

    (App2) Created a simple UIKit application by means of the Xcode "Create project" template.

---
+++ App2/App2/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  App2
+//
+//  Created by Adil Hussain on 24/05/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+    }
+}
